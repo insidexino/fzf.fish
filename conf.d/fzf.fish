23b0ac1be376471cbd48c9033b7991f0bdcb3b8f
Patrick Fong <patrickf3139@gmail.com>
2020-08-11 10:49:56 -0700

    simplify conf

--- conf.d/fzf.fish
+++ conf.d/fzf.fish
@@ -1,16 +1,18 @@
-# Set up the default, mnemonic keybindings
-# \cf is ctrl+f, etc.
-bind \cf '__fzf_search_current_dir'
-bind \cl '__fzf_search_git_log'
-bind \cr '__fzf_search_history'
-bind \cv '__fzf_search_shell_variables'
+# Set up the default, mnemonic keybindings unless the user has chosen to customize them
+if not set --query fzf_fish_custom_keybindings
+    # \cf is ctrl+f, etc.
+    bind \cf '__fzf_search_current_dir'
+    bind \cl '__fzf_search_git_log'
+    bind \cr '__fzf_search_history'
+    bind \cv '__fzf_search_shell_variables'
 
-# set up the same keybindings for insert mode if using fish_vi_key_bindings
-if [ $fish_key_bindings = 'fish_vi_key_bindings' ]
-    bind --mode insert \cf '__fzf_search_current_dir'
-    bind --mode insert \cl '__fzf_search_git_log'
-    bind --mode insert \cr '__fzf_search_history'
-    bind --mode insert \cv '__fzf_search_shell_variables'
+    # set up the same keybindings for insert mode if using fish_vi_key_bindings
+    if [ $fish_key_bindings = 'fish_vi_key_bindings' ]
+        bind --mode insert \cf '__fzf_search_current_dir'
+        bind --mode insert \cl '__fzf_search_git_log'
+        bind --mode insert \cr '__fzf_search_history'
+        bind --mode insert \cv '__fzf_search_shell_variables'
+    end
 end
 
 # If FZF_DEFAULT_OPTS is not set, then set some sane defaults. This also affects fzf outside of this plugin.
@@ -23,7 +25,8 @@ if not set --query FZF_DEFAULT_OPTS
     set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height 75%'
 end
 
-# https://fishshell.com/docs/current/#event
+# In case the user sets fzf_fish_custom_keybindings in their config.fish AFTER the default keybindings
+# have already been set, set up erase_default_bindings to detect this and erase them
 function erase_default_bindings --on-variable fzf_fish_custom_keybindings
     bind --erase --all \cf
     bind --erase --all \cl
